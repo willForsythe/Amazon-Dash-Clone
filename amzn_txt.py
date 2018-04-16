5d903fdd1daf1e9d2c142c47e5bd759ac6e8334b
willForsythe <wforsythe65@gmail.com>
2018-04-16 09:33:27 -0700

    Update amzn_txt.py

--- amzn_txt.py
+++ amzn_txt.py
@@ -3,18 +3,18 @@ from twilio.rest import Client
 
 print("Listening...\n")
 
-account = "AC5c1ecd299238426828c355d1a21513ec"
-token = "2418b655ee3810993b6e94b74d0d496f"
+account = "twilio_acct"
+token = "twilio_token"
 client = Client(account, token)
 
 
 def arp_display(pkt):
     if pkt[ARP].op == 1:
-        if pkt[ARP].hwsrc == "44:32:c8:f3:10:07":
-            print("ARP Probe from: 44:32:c8:f3:10:07" + pkt[ARP].hwsrc + "\n")
+        if pkt[ARP].hwsrc == "mac_address":
+            print("ARP Probe from: " + pkt[ARP].hwsrc + "\n")
             message = client.messages.create(
-                to="+17073732510",
-                from_="+14153606734",
+                to="#",
+                from_="twilio_#",
                 body="I NEED HELP!")
             print("Message sent \n")
 
